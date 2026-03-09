29951f3d1b75abbaed8a49ff94f544152cbde98e
James Power <jep@lepoer.us>
2026-03-09 16:57:32 -0400

    Delete unused custom views: ListRowView, ColorCompat, RealTranslateAnimation.

--- BTLib/src/com/offsetnull/bt/ui/ColorCompat.java
+++
@@ -1,9 +0,0 @@
-package com.offsetnull.bt.ui;
-
-public class ColorCompat {
-
-    public static int argb(int a, int r, int g, int b) {
-        return android.graphics.Color.argb(a,r,g,b);
-    }
-
-}
