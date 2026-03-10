60c45504b4965217296616344b2abd4706330daf
James Power <jep@lepoer.us>
2026-03-10 18:33:26 -0400

    Add DataStore dependency and convert callback interfaces to Kotlin.

---
+++ BTLib/src/main/kotlin/com/offsetnull/bt/service/SettingsChangedListener.kt
@@ -0,0 +1,10 @@
+package com.offsetnull.bt.service
+
+interface SettingsChangedListener {
+    fun updateSetting(key: String, value: String)
+}
+
+/** Sealed event hierarchy for future Flow-based replacement of [SettingsChangedListener]. */
+sealed interface SettingsEvent {
+    data class SettingChanged(val key: String, val value: String) : SettingsEvent
+}
