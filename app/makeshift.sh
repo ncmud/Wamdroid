75841633a29c773499605d8b04cd1ce2c2b45d59
James Power <jep@lepoer.us>
2026-03-11 23:31:03 -0400

    Fix remaining BlowTorch references in legacy build scripts and Lua assets.

--- app/makeshift.sh
+++ app/makeshift.sh
@@ -4,5 +4,5 @@ cp -rv ../../btscripts/button_ex/src/ ./assets/share/lua/5.1/
 rm -rf ./assets/share/lua/5.1/button_window_ex.xml 
 python integrate_button_ex.py 
 ant release
-adb install -r bin/BlowTorch-release.apk
+adb install -r bin/MUDWammer-release.apk
 adb shell am start -n com.happygoatstudios.bt/com.offsetnull.btfree.FreeLauncher 
